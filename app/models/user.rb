15af33e914e7843695ecbaa1027f6a222feaf5ed
yinwai92 <wanyw92@gmail.com>
2015-05-16 14:54:06 +0800

    Finish setup Dropbox

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,22 @@
 class User < ActiveRecord::Base
-	has_attached_file 	:profilepic, 
+	if Rails.env.development?
+		# development environment
+		has_attached_file 	:profilepic, 
 						:styles => { :medium => "250x>", :thumb => "100x100>" }, 
-						:default_url => "avatar_default.png",
-						:storage => :dropbox,
-    					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
-    					
-  	validates_attachment_content_type 	:profilepic, 
-  										:content_type => /\Aimage\/.*\Z/
-  					
+						:default_url => "avatar_default.png"
+
+		validates_attachment_content_type 	:profilepic, 
+	  										:content_type => /\Aimage\/.*\Z/
+    else
+    	# production environment
+		has_attached_file 	:profilepic, 
+							:styles => { :medium => "250x>", :thumb => "100x100>" }, 
+							:default_url => "avatar_default.png",
+							:storage => :dropbox,
+	    					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	    					:path => ":style/:id_:filename" # determine uploaded image size (medium, thumb), stored with name "id_extension"
+	    					
+	  	validates_attachment_content_type 	:profilepic, 
+	  										:content_type => /\Aimage\/.*\Z/
+  	end				
 end
